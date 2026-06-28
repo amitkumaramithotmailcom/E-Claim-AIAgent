f619f8fd3556d83efce128247679010afe70895a
Amit Kumar <amitkumaramit@hotmail.com>
2026-06-28 16:02:00 +0530

    Remove App files

--- EClaim.BlazorApp/EClaim.BlazorApp/Models/ClaimDocumentModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EClaim.Application.Models
-{
-    public class ClaimDocumentModel
-    {
-        public string FileName { get; set; }
-        public string FilePath { get; set; }
-    }
-}
--- EClaim.BlazorApp/EClaim.BlazorApp/Models/ViewModel/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EClaim.Application.Models.ViewModel
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
